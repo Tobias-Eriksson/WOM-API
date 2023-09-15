983ccbc82773c8eed80322bfb08b26a566c43a2e
Tobias Eriksson <tobias.eriksson@elisa.fi>
2023-09-15 15:49:25 +0300

    Made editable notes that send info to DB

--- Front-end/scripts/notes.ts
+++ Front-end/scripts/notes.ts
@@ -1,17 +1,53 @@
 const notesDiv = <HTMLElement>document.getElementById("notesDiv")
+const boardsDiv = document.getElementById("boards")
+const newNoteBtn = document.getElementById("newNoteBtn");
+
+function updateNote(noteText: string, id: string) {
+    const headers: Headers = new Headers()
+    headers.set('Content-Type', 'application/json')
+    headers.set('Accept', 'application/json')
+    headers.set('X-Custom-Header', 'CustomValue')
+
+    const patchData = {
+        text: noteText
+    }
+    const request: RequestInfo = new Request('http://localhost:3000/notes/' + id, {
+        method: 'PATCH',
+        headers: headers,
+        body: JSON.stringify(patchData)
+    })
+    return fetch(request)
+}
+
+function newNote() {
+    const headers: Headers = new Headers()
+    headers.set('Content-Type', 'application/json')
+    headers.set('Accept', 'application/json')
+    headers.set('X-Custom-Header', 'CustomValue')
+
+    const postData = {
+        text: 'Placeholder',
+    };
+
+    const request: RequestInfo = new Request('http://localhost:3000/notes', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(postData)
+    })
+    window.location.reload();
+    return fetch(request)
+
+}
 
 interface Notes<T> {
     msg: string
     reqBody: T[];
 }
-
 interface reqBody {
     id: string,
     noteText: string
 }
-
 function getNotes(): Promise<Notes<reqBody>> {
-
     const headers: Headers = new Headers()
     headers.set('Content-Type', 'application/json')
     headers.set('Accept', 'application/json')
@@ -33,17 +69,24 @@ try {
     getNotes()
         .then(notes => {
             for (let i = 0; i < notes.reqBody.length; i++) {
-                const note = <HTMLElement>document.createElement("div")
+                const note = <HTMLElement>document.createElement("blockquote")
+                note.contentEditable = "true"
                 note.id = "note" + i
-                note.classList.add('notes', 'col-sm-2', 'm-2');
+                note.classList.add('notes', 'col-sm-2', 'm-2', 'justify-content-center');
                 note.innerHTML = notes.reqBody[i].noteText
-
+                note.oninput = function () {
+                    updateNote(note.innerText.toString(), (notes.reqBody[i].id).toString())
+                }
                 notesDiv.appendChild(note);
 
             }
         })
 
-}catch(err){
+} catch (err) {
     console.log(err)
 }
 
+
+if (newNoteBtn) {
+    newNoteBtn.addEventListener("click", newNote);
+}
