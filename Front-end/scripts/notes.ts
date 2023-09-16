491bab3b544a77eba6d2973443f71b282ca5308a
Tobias Eriksson <tobias.eriksson@elisa.fi>
2023-09-16 15:51:39 +0300

    Added remove button and functionality, comments

--- Front-end/scripts/notes.ts
+++ Front-end/scripts/notes.ts
@@ -1,7 +1,14 @@
 const notesDiv = <HTMLElement>document.getElementById("notesDiv")
 const boardsDiv = document.getElementById("boards")
-const newNoteBtn = document.getElementById("newNoteBtn");
+const newNoteBtn = document.getElementById("newNoteBtn")
+const deleteBtns = document.getElementsByClassName("xButton")
 
+//Ny note knapp eventlistener
+if (newNoteBtn) {
+    newNoteBtn.addEventListener("click", newNote);
+}
+
+//Uppdaterar note texten
 function updateNote(noteText: string, id: string) {
     const headers: Headers = new Headers()
     headers.set('Content-Type', 'application/json')
@@ -19,6 +26,7 @@ function updateNote(noteText: string, id: string) {
     return fetch(request)
 }
 
+//Gör en ny note
 function newNote() {
     const headers: Headers = new Headers()
     headers.set('Content-Type', 'application/json')
@@ -39,6 +47,8 @@ function newNote() {
 
 }
 
+//Interfaces som behövs till vad getNotes() kan förvänta sig av response struktern
+//https://www.sohamkamani.com/typescript/rest-http-api-call/
 interface Notes<T> {
     msg: string
     reqBody: T[];
@@ -47,6 +57,8 @@ interface reqBody {
     id: string,
     noteText: string
 }
+
+//Get alla notes
 function getNotes(): Promise<Notes<reqBody>> {
     const headers: Headers = new Headers()
     headers.set('Content-Type', 'application/json')
@@ -65,28 +77,62 @@ function getNotes(): Promise<Notes<reqBody>> {
         })
 }
 
-try {
-    getNotes()
-        .then(notes => {
-            for (let i = 0; i < notes.reqBody.length; i++) {
-                const note = <HTMLElement>document.createElement("blockquote")
-                note.contentEditable = "true"
-                note.id = "note" + i
-                note.classList.add('notes', 'col-sm-2', 'm-2', 'justify-content-center');
-                note.innerHTML = notes.reqBody[i].noteText
-                note.oninput = function () {
-                    updateNote(note.innerText.toString(), (notes.reqBody[i].id).toString())
+//Remove en note med id
+function removeNote(id: string, event: MouseEvent) {
+    const headers: Headers = new Headers()
+    headers.set('Content-Type', 'application/json')
+    headers.set('Accept', 'application/json')
+    headers.set('X-Custom-Header', 'CustomValue')
+
+    const request: RequestInfo = new Request('http://localhost:3000/notes/' + id, {
+        method: 'DELETE',
+        headers: headers
+    })
+    window.location.reload();
+    return fetch(request)
+}
+
+//Hämtar alla notes med getNotes() och formaterar dom till enskillda notes
+//HTML Strukturen blir notesDiv>note>noteText,xButton
+//Grid strukturen är gjort med bootstrap classes t.ex col-sm-10
+function displayNotes() {
+    try {
+        getNotes()
+            .then(notes => {
+                for (let i = 0; i < notes.reqBody.length; i++) {
+                    const note = <HTMLElement>document.createElement("div")
+                    note.classList.add('notes', 'col-sm-2', 'm-2', 'justify-content-center');
+                    note.setAttribute('id', (notes.reqBody[i].id).toString())
+
+                    const noteText = <HTMLElement>document.createElement("blockquote")
+                    noteText.contentEditable = "true"
+                    noteText.id = "note" + i
+                    noteText.classList.add('notesText', 'col-sm-10', 'justify-content-center');
+                    noteText.innerHTML = notes.reqBody[i].noteText
+                    noteText.setAttribute('id', (notes.reqBody[i].id).toString())
+                    noteText.oninput = function () {
+                        updateNote(noteText.innerText.toString(), (notes.reqBody[i].id).toString())
+                    }
+
+                    const xButton = <HTMLElement>document.createElement("button")
+                    xButton.id = "xButton" + i
+                    xButton.classList.add('xButton', 'btn-close', 'col-sm-1')
+                    xButton.setAttribute('id', (notes.reqBody[i].id).toString())
+
+                    xButton.addEventListener('click', (event) => {
+                        removeNote(xButton.id, event);
+                    })
+
+                    note.appendChild(noteText)
+                    note.appendChild(xButton)
+                    notesDiv.appendChild(note)
                 }
-                notesDiv.appendChild(note);
+            })
 
-            }
-        })
-
-} catch (err) {
-    console.log(err)
+    } catch (err) {
+        console.log(err)
+    }
 }
 
-
-if (newNoteBtn) {
-    newNoteBtn.addEventListener("click", newNote);
-}
+//Ladda in alla notes
+displayNotes()
